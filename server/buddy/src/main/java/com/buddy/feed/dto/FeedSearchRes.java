8f10eb7fd6806655ed0a8b0c0c8a375fecd09f4f
dokkyunYU <ydgar001@naver.com>
2023-04-24 22:16:57 +0900

    feat : 특정 문자로 시작하는 유저 반환

---
+++ server/buddy/src/main/java/com/buddy/feed/dto/FeedSearchRes.java
@@ -0,0 +1,17 @@
+package com.buddy.feed.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FeedSearchRes {
+    private List<String> nickname;
+}
+
