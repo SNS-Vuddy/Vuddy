ba696e24e95e46a2d59f37ac5b5cd518903f9306
류원창 <wom05169@gmail.com>
2023-04-25 17:42:23 +0900

    feat: 유저 엔티티 수정

--- server/buddy/src/main/java/com/buddy/model/entity/User.java
+++ server/buddy/src/main/java/com/buddy/model/entity/User.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import javax.persistence.*;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotEmpty;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -17,14 +18,31 @@ public class User {
     private Long id;
 
     @Max(value = 20, message = "닉네임은 20자 이하로 입력해주세요.")
+    @NotEmpty(message = "닉네임이 입력되지 않았습니다.")
+    @Column(name = "u_nickname")
     private String nickname;
 
+    @NotEmpty(message = "이메일이 입력되지 않았습니다.")
+    @Column(name = "u_email")
+    private String email;
+
     @NotEmpty(message = "비밀번호가 입력되지 않았습니다.")
+    @Column(name = "u_password")
     private String password;
 
-    @Column(name = "profile_image")
+    @Column(name = "u_profile_img")
     private String profileImage;
 
-    @Column(name = "status_message")
+    @Column(name = "u_status_message")
     private String statusMessage;
+
+    @Column(name = "u_created_at")
+    private LocalDateTime createdAt;
+    
+    @Column(name = "u_withdrawal_at")
+    private LocalDateTime withdrawalAt;
+
+    @Column(name = "u_is_withdrawal")
+    private boolean isWithdrawal;
+
 }
