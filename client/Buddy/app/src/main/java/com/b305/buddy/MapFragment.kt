12bc44ac5d3b80923a7ec4beb0c4678922ad8678
HyunseokCheong <hyunseokcheong@gmail.com>
2023-04-27 11:28:59 +0900

    feat: Location Provider 구현

--- client/Buddy/app/src/main/java/com/b305/buddy/MapFragment.kt
+++ client/Buddy/app/src/main/java/com/b305/buddy/MapFragment.kt
@@ -1,35 +1,31 @@
 package com.b305.buddy
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
 
-class MapFragment : Fragment(), OnMapReadyCallback {
-    
-    private var mMap: GoogleMap? = null
+class MapFragment : Fragment() {
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
     
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? { // 구글맵 쓰려면 SHA-1 인증서랑, 패키지 이름 등록해야함
-        // 이 페이지에서 nullpointerexception 발생하면 이거 의심해 봐야함
-        // https://console.cloud.google.com/apis/credentials/key/e0832e83-9623-49c5-af00-c04c27585abf?hl=ko&project=airquality-384811
-        // 윈도우
-        // "C:\Program Files\Android\Android Studio\jre\bin\keytool" -list -v -keystore "%USERPROFILE%\.android\debug.keystore" -alias androiddebugkey -storepass android -keypass android
         
-        val mapFragment = this.childFragmentManager.findFragmentById(R.id.fg_map) as SupportMapFragment?
-        mapFragment?.getMapAsync(this)
-        
+        // navigation
         val view = inflater.inflate(R.layout.fragment_map, container, false)
         
+        // 임시 이동 버튼
+        view.findViewById<Button>(R.id.btn_move_to_map).setOnClickListener {
+            val intent = Intent(activity, MapActivity::class.java)
+            startActivity(intent)
+        }
         view.findViewById<ImageView>(R.id.iv_friend).setOnClickListener {
             it.findNavController().navigate(R.id.action_mapFragment_to_friendFragment)
         }
@@ -41,9 +37,5 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         }
         
         return view
-    }
-    
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
     }
 }
