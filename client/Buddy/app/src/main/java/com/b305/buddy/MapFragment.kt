6be8a591e9ff80f04b17fe212bcf9923f8fbe842
HyunseokCheong <hyunseokcheong@gmail.com>
2023-04-27 13:15:59 +0900

    feat: 친구 마커 구현

--- client/Buddy/app/src/main/java/com/b305/buddy/MapFragment.kt
+++ client/Buddy/app/src/main/java/com/b305/buddy/MapFragment.kt
@@ -1,7 +1,6 @@
 package com.b305.buddy
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,6 +12,7 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 class MapFragment : Fragment(), OnMapReadyCallback {
     
@@ -26,7 +26,7 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         super.onCreate(savedInstanceState)
     }
     
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? { // 구글맵 쓰려면 SHA-1 인증서랑, 패키지 이름 등록해야함
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         
         binding = FragmentMapBinding.inflate(layoutInflater, container, false)
         
@@ -40,6 +40,7 @@ class MapFragment : Fragment(), OnMapReadyCallback {
             it.findNavController().navigate(R.id.action_mapFragment_to_profileFragment)
         }
         
+        setButton()
         return binding.root
     }
     
@@ -52,12 +53,83 @@ class MapFragment : Fragment(), OnMapReadyCallback {
     
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-        Log.d("MapFragment", "onMapReady")
         
+        getLocation()
+    }
+    
+    private fun setButton() {
+        binding.fabCurrentLocation.setOnClickListener {
+            getLocation()
+        }
+    }
+    
+    private fun getLocation() {
         val locationProvider = LocationProvider(requireActivity())
-        val currentLat = locationProvider.getLocationLatitude()
-        val currentLng = locationProvider.getLocationLongitude()
         
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(currentLat!!, currentLng!!), 16f))
+        currentLat = locationProvider.getLocationLatitude()!!
+        currentLng = locationProvider.getLocationLongitude()!!
+        
+        setMarker()
+        
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(currentLat, currentLng), 16f))
+    }
+    
+    private fun setMarker() {
+        // 친구 테스트용
+        val f1Lat: Double = 36.3507133
+        val f1Lng: Double = 127.2986109
+        val f2Lat: Double = 36.3599459
+        val f2Lng: Double = 127.3051566
+        val f3Lat: Double = 36.3616414
+        val f3Lng: Double = 127.3575561
+        val friend1 = Friend()
+        friend1.nickname = 1
+        friend1.lat = f1Lat
+        friend1.lng = f1Lng
+        val friend2 = Friend()
+        friend2.nickname = 2
+        friend2.lat = f2Lat
+        friend2.lng = f2Lng
+        val friend3 = Friend()
+        friend3.nickname = 3
+        friend3.lat = f3Lat
+        friend3.lng = f3Lng
+        
+        val friendList: ArrayList<Friend> = ArrayList()
+        friendList.add(friend1)
+        friendList.add(friend2)
+        friendList.add(friend3)
+        
+        mMap.let {
+            it.clear()
+            val markerOption = MarkerOptions()
+            markerOption.position(LatLng(currentLat, currentLng))
+            val marker = it.addMarker(markerOption)
+            
+            // 친구 테스트용
+            for (friend in friendList) {
+                val friendMarkerOption = MarkerOptions()
+                friendMarkerOption.position(friend.getLocation())
+                val friendMarker = it.addMarker(friendMarkerOption)
+            }
+        }
+    }
+    
+    inner class Friend {
+        var nickname: Long? = null
+        var lat: Double? = null
+        var lng: Double? = null
+        
+        fun getLatitude(): Double { // 수정된 이름
+            return lat ?: 0.0
+        }
+        
+        fun getLongitude(): Double {
+            return lng ?: 0.0
+        }
+        
+        fun getLocation(): LatLng {
+            return LatLng(lat!!, lng!!)
+        }
     }
 }
