ff311b3987ccdc29bc67d40eee04df1ca45ef021
HyunseokCheong <hyunseokcheong@gmail.com>
2023-04-27 12:52:10 +0900

    feat: 지도와 네비게이션 바 통합

--- client/Buddy/app/src/main/java/com/b305/buddy/MapFragment.kt
+++ client/Buddy/app/src/main/java/com/b305/buddy/MapFragment.kt
@@ -1,16 +1,26 @@
 package com.b305.buddy
 
-import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import com.b305.buddy.databinding.FragmentMapBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
+    
+    lateinit var binding: FragmentMapBinding
+    lateinit var mMap: GoogleMap
+    
+    var currentLat: Double = 37.4979769 // default: 강남역
+    var currentLng: Double = 127.027729
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,24 +28,36 @@ class MapFragment : Fragment() {
     
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? { // 구글맵 쓰려면 SHA-1 인증서랑, 패키지 이름 등록해야함
         
-        // navigation
-        val view = inflater.inflate(R.layout.fragment_map, container, false)
+        binding = FragmentMapBinding.inflate(layoutInflater, container, false)
         
-        // 임시 이동 버튼
-        view.findViewById<Button>(R.id.btn_move_to_map).setOnClickListener {
-            val intent = Intent(activity, MapActivity::class.java)
-            startActivity(intent)
-        }
-        view.findViewById<ImageView>(R.id.iv_friend).setOnClickListener {
+        binding.ivFriend.setOnClickListener {
             it.findNavController().navigate(R.id.action_mapFragment_to_friendFragment)
         }
-        view.findViewById<ImageView>(R.id.iv_message).setOnClickListener {
+        binding.ivMessage.setOnClickListener {
             it.findNavController().navigate(R.id.action_mapFragment_to_messageFragment)
         }
-        view.findViewById<ImageView>(R.id.iv_profile).setOnClickListener {
+        binding.ivProfile.setOnClickListener {
             it.findNavController().navigate(R.id.action_mapFragment_to_profileFragment)
         }
         
-        return view
+        return binding.root
+    }
+    
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        
+        val supportMapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        supportMapFragment.getMapAsync(this)
+    }
+    
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        Log.d("MapFragment", "onMapReady")
+        
+        val locationProvider = LocationProvider(requireActivity())
+        val currentLat = locationProvider.getLocationLatitude()
+        val currentLng = locationProvider.getLocationLongitude()
+        
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(currentLat!!, currentLng!!), 16f))
     }
 }
