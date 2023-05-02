e5f313829f15b56b875aa41fd692e79f240842a9
이진욱 <dlwls9130@naver.com>
2023-05-02 17:39:17 +0900

    feat: 친구목록 리ã리사이클뷰

--- client/Buddy/app/src/main/java/com/b305/buddy/FriendFragment.kt
+++ client/Buddy/app/src/main/java/com/b305/buddy/FriendFragment.kt
@@ -7,12 +7,31 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class FriendFragment : Fragment() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
-    
+
+    private lateinit var profileAdapter: ProfileAdapter
+    private lateinit var recyclerView: RecyclerView
+
+    private val profileList: ArrayList<Profiles> = arrayListOf(
+        Profiles(R.drawable.man, "user1", "status1"),
+        Profiles(R.drawable.woman, "user2", "status2"),
+        Profiles(R.drawable.man, "user3", "status3"),
+        Profiles(R.drawable.woman, "user4", "status4"),
+        Profiles(R.drawable.man, "user5", "status5"),
+        Profiles(R.drawable.woman, "user6", "status6"),
+        Profiles(R.drawable.man, "user7", "status7"),
+        Profiles(R.drawable.woman, "user8", "status8"),
+        Profiles(R.drawable.man, "user9", "status9"),
+        Profiles(R.drawable.woman, "user10", "status10"),
+    )
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         
         val view = inflater.inflate(R.layout.fragment_friend, container, false)
@@ -30,5 +49,16 @@ class FriendFragment : Fragment() {
         }
         
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val layoutManager = LinearLayoutManager(context)
+        recyclerView = view.findViewById(R.id.rv_profile)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.setHasFixedSize(true)
+        profileAdapter = ProfileAdapter(profileList)
+        recyclerView.adapter = profileAdapter
     }
 }
