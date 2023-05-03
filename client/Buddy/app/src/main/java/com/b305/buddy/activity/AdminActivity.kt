52ff5e237aa8d5eada5284ad2e67e9ea2d4d6a8e
HyunseokCheong <hyunseokcheong@gmail.com>
2023-05-03 23:48:47 +0900

    feat: add editor config

--- client/Buddy/app/src/main/java/com/b305/buddy/activity/AdminActivity.kt
+++ client/Buddy/app/src/main/java/com/b305/buddy/activity/AdminActivity.kt
@@ -15,27 +15,27 @@ import kotlinx.coroutines.Runnable
 // admin
 // adminadmin
 class AdminActivity : AppCompatActivity() {
-    
+
     lateinit var binding: ActivityAdminBinding
     private val sharedManager: SharedManager by lazy { SharedManager(this) }
     private val socket = Socket(this)
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAdminBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        
+
         setButton()
     }
-    
+
     private fun sendLocation() {
         val locationProvider = LocationProvider(this)
         val latitude = locationProvider.getLocationLatitude().toString()
         val longitude = locationProvider.getLocationLongitude().toString()
-        
+
         socket.sendLocation(latitude, longitude)
     }
-    
+
     private fun setButton() {
         // 로그아웃 버튼
         binding.btnLogout.setOnClickListener {
@@ -46,13 +46,13 @@ class AdminActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
-        
+
         // 소켓 연결 버튼
         binding.btnConnect.setOnClickListener {
             Log.d("AdminActivity", "connection")
             socket.connection()
         }
-        
+
         val handler = Handler()
         val runnable = object : Runnable {
             override fun run() {
@@ -61,32 +61,32 @@ class AdminActivity : AppCompatActivity() {
                 handler.postDelayed(this, 3000)
             }
         }
-        
+
         // 소켓 인터벌 전송 버튼
         binding.btnSend.setOnClickListener {
             handler.post(runnable)
         }
-        
+
         // 소켓 인터벌 전송 정지 버튼
         binding.btnStop.setOnClickListener {
             handler.removeCallbacks(runnable)
         }
-        
+
         // 이모탈 시작 버튼
         binding.btnStartImmortal.setOnClickListener {
             Log.d("AdminActivity", "Click Immortal Button")
         }
-        
+
         // 서비스 A 정지 버튼
         binding.btnEndA.setOnClickListener {
             Log.d("AdminActivity", "Click EndA Button")
         }
-        
+
         // 서비스 B 정지 버튼
         binding.btnEndB.setOnClickListener {
             Log.d("AdminActivity", "Click EndB Button")
         }
-        
+
         // 서비스 둘다 정지 버튼
         binding.btnEndBoth.setOnClickListener {
             Log.d("AdminActivity", "Click EndBoth Button")
