2f8db7a17e8c05337ce01fd7eeeeb614a8e26d48
류원창 <wom05169@gmail.com>
2023-05-04 11:18:28 +0900

    init: user dev 서버 분리

---
+++ src/test/java/com/edu/ssafy/user/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.ssafy.user.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
