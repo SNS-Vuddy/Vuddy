0e2902bf7df0ddb06d0d0f1fbfb42240f4d96311
류원창 <wom05169@gmail.com>
2023-05-04 15:33:05 +0900

    init: comment 서버 분리

---
+++ src/main/java/com/edu/ssafy/comment/CommentApplication.java
@@ -0,0 +1,25 @@
+package com.edu.ssafy.comment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import java.util.Base64;
+
+@SpringBootApplication
+public class CommentApplication {
+
+    @GetMapping("/comment/hello")
+    public String hello(@RequestHeader("x-forwarded-for-nickname") String nickname) {
+        //Base64 로 nickname 디코딩하기
+        String userNickname = new String(Base64.getDecoder().decode(nickname));
+
+        return userNickname;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommentApplication.class, args);
+    }
+
+}
