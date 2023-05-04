0ee43bad394df585729514a3bc2e5b2936d41421
류원창 <wom05169@gmail.com>
2023-05-04 17:48:27 +0900

    feat: user 서버 분리 작업 중

--- src/main/java/com/edu/ssafy/user/controller/UserController.java
+++ src/main/java/com/edu/ssafy/user/controller/UserController.java
@@ -3,6 +3,7 @@ package com.edu.ssafy.user.controller;
 import com.edu.ssafy.user.model.dto.common.CommonRes;
 import com.edu.ssafy.user.model.dto.common.SingleRes;
 import com.edu.ssafy.user.model.dto.response.UserFeedsSummaryRes;
+import com.edu.ssafy.user.model.dto.response.UserProfileWithFeedsRes;
 import com.edu.ssafy.user.model.entity.User;
 import com.edu.ssafy.user.model.service.UserService;
 import com.edu.ssafy.user.util.NicknameUtil;
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +29,16 @@ public class UserController {
         User user = userService.findById(nickname);
         UserFeedsSummaryRes myUserWithFeedsDto = userService.findUserAndFeeds(user);
         return new SingleRes<>(200, "유저 정보 조회 성공", myUserWithFeedsDto);
+    }
+
+    // 특정 유저 프로필, 피드들 조회
+    @GetMapping("/profile/{targetUserNickname}")
+    public CommonRes getUserInfoAndFeeds(@RequestHeader("x-forwarded-for-nickname") String encodeNickname, @PathVariable String targetUserNickname) {
+        String userNickname = NicknameUtil.decodeNickname(encodeNickname);
+
+        UserProfileWithFeedsRes userProfileWithFeedsRes = userService.findUsersWithFriendStatus(userNickname, targetUserNickname);
+
+        return new SingleRes<>(200, "유저 정보 조회 성공", userProfileWithFeedsRes);
     }
 
 
--- src/main/java/com/edu/ssafy/user/model/service/UserService.java
+++ src/main/java/com/edu/ssafy/user/model/service/UserService.java
@@ -2,6 +2,7 @@ package com.edu.ssafy.user.model.service;
 
 import com.edu.ssafy.user.model.dto.BriefFeedIngoDto;
 import com.edu.ssafy.user.model.dto.response.UserFeedsSummaryRes;
+import com.edu.ssafy.user.model.dto.response.UserProfileWithFeedsRes;
 import com.edu.ssafy.user.model.entity.User;
 import com.edu.ssafy.user.model.repository.FeedRepository;
 import com.edu.ssafy.user.model.repository.UserRepository;
@@ -10,6 +11,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -26,5 +30,29 @@ public class UserService {
     public UserFeedsSummaryRes findUserAndFeeds(User user) {
         List<BriefFeedIngoDto> briefFeedIngoDtoList = feedRepository.findAllBriefInfoByUserId(user.getId());
         return new UserFeedsSummaryRes(user.getNickname(), user.getProfileImage(), user.getStatusMessage(), briefFeedIngoDtoList);
+    }
+
+    public UserProfileWithFeedsRes findUsersWithFriendStatus(String myNickname, String userNickname) {
+
+        List<User> users = findAllByNicknameIn(List.of(myNickname, userNickname));
+
+        Map<String, User> userMap = users.stream().collect(Collectors.toMap(User::getNickname, Function.identity()));
+        User myUser = userMap.get(myNickname);
+        User targetUser = userMap.get(userNickname);
+
+        String friendStatus = userRepository.existsByMyUserNicknameAndTargetUserNickname(myNickname, userNickname);
+
+        UserFriendshipStatusDto userFriendshipStatusDto = new UserFriendshipStatusDto(myUser, targetUser, friendStatus);
+
+        List<BriefFeedIngoDto> briefFeedIngoDtoList = feedRepository.findAllBriefInfoByUserId(myUser.getId());
+
+        return UserProfileWithFeedsRes.builder()
+                .nickname(userFriendshipStatusDto.getMyUser().getNickname())
+                .profileImage(userFriendshipStatusDto.getMyUser().getProfileImage())
+                .statusMessage(userFriendshipStatusDto.getMyUser().getStatusMessage())
+                .feeds(briefFeedIngoDtoList)
+                .canISeeFeeds(true)
+                .isFriend(friendStatus)
+                .build();
     }
 }
