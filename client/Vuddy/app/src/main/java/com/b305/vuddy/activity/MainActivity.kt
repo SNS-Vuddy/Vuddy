64aa05238614b5fa1b555e561aea22c08074e993
HyunseokCheong <hyunseokcheong@gmail.com>
2023-05-11 10:41:19 +0900

    feat: 개선된 지도와 이모탈 서비스 구현

--- client/Vuddy/app/src/main/java/com/b305/vuddy/activity/MainActivity.kt
+++ client/Vuddy/app/src/main/java/com/b305/vuddy/activity/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.b305.vuddy.activity
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -12,17 +14,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         startService(Intent(this, TestService::class.java))
-//        startService(Intent(this, ImmortalLocationService::class.java))
     }
 
-//    companion object {
-//        class BootReceiver : BroadcastReceiver() {
-//            override fun onReceive(context: Context, intent: Intent) {
-//                if ("android.intent.action.BOOT_COMPLETED" == intent.action) {
-//                    val serviceIntent = Intent(context, ImmortalLocationService::class.java)
-//                    context.startService(serviceIntent)
-//                }
-//            }
-//        }
-//    }
+    companion object {
+        class BootReceiver : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                if ("android.intent.action.BOOT_COMPLETED" == intent.action) {
+                    val serviceIntent = Intent(context, TestService::class.java)
+                    context.startService(serviceIntent)
+                }
+            }
+        }
+    }
 }
--- client/Vuddy/app/src/main/java/com/b305/vuddy/service/TestService.kt
+++ client/Vuddy/app/src/main/java/com/b305/vuddy/service/TestService.kt
@@ -1,16 +1,24 @@
 package com.b305.vuddy.service
 
 import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Intent
+import android.graphics.Color
 import android.os.Looper
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
 import androidx.lifecycle.MutableLiveData
+import com.b305.vuddy.R
+import com.b305.vuddy.activity.MainActivity
 import com.b305.vuddy.model.LocationEvent
 import com.b305.vuddy.model.UserLocation
 import com.b305.vuddy.util.FASTEST_LOCATION_UPDATE_INTERVAL
 import com.b305.vuddy.util.LOCATION_UPDATE_INTERVAL
+import com.b305.vuddy.util.LocationSocket
+import com.b305.vuddy.util.NOTI_ID
 import com.b305.vuddy.util.SharedManager
 import com.b305.vuddy.util.TrackingUtility
 import com.google.android.gms.location.FusedLocationProviderClient
@@ -22,10 +30,8 @@ import org.greenrobot.eventbus.EventBus
 
 class TestService : LifecycleService() {
     lateinit var fusedLocationProviderClient: FusedLocationProviderClient
-
-    //    lateinit var baseNotificationBuilder: NotificationCompat.Builder
-//    lateinit var currentNotificationBuilder: NotificationCompat.Builder
-    private val sharedManager: SharedManager by lazy { SharedManager(applicationContext) }
+    private val sharedManager: SharedManager by lazy { SharedManager(this) }
+    private lateinit var locationSocket: LocationSocket
 
     // 선언
     companion object {
@@ -39,7 +45,7 @@ class TestService : LifecycleService() {
 
     override fun onCreate() {
         super.onCreate()
-//        currentNotificationBuilder = baseNotificationBuilder
+        createNotification()
         postInitialValues()
 
         isTracking.observe(this) {
@@ -75,15 +81,51 @@ class TestService : LifecycleService() {
     private val locationCallback = object : LocationCallback() {
         override fun onLocationResult(result: LocationResult) {
             super.onLocationResult(result)
+
+            if (!::locationSocket.isInitialized) {
+                locationSocket = LocationSocket(applicationContext)
+                locationSocket.connection()
+            }
+
             val location = result.locations.last()
             Log.d("****", "${location.latitude}, ${location.longitude}")
-
             val userLocation = UserLocation()
             val latitude = location.latitude.toString()
             val longitude = location.longitude.toString()
             userLocation.lat = latitude
             userLocation.lng = longitude
+            locationSocket.sendLocation(latitude, longitude)
             EventBus.getDefault().post(LocationEvent(true, userLocation))
         }
+    }
+
+    private fun createNotification() {
+        val builder = NotificationCompat.Builder(this, "default")
+        builder.setSmallIcon(R.mipmap.ic_launcher)
+        builder.setContentTitle("Vuddy")
+        builder.setContentText("친구들에게 내 위치를 공유중입니다.")
+        builder.color = Color.RED
+        val notificationIntent = Intent(this, MainActivity::class.java)
+        notificationIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_MUTABLE)
+        builder.setContentIntent(pendingIntent)
+
+        // 알림 표시
+        val notificationManager = this.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(
+            NotificationChannel(
+                "default",
+                "기본 채널",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+        )
+        notificationManager.notify(NOTI_ID, builder.build()) // id : 정의해야하는 각 알림의 고유한 int값
+        val notification = builder.build()
+        startForeground(NOTI_ID, notification)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        locationSocket.disconnect()
     }
 }
