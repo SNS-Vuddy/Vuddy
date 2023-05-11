9eb825dfda9cb6be5d3c162d3758971bcfe62207
dokkyunYU <ydgar001@naver.com>
2023-05-11 10:17:22 +0900

    feat : health 추가

---
+++ src/main/java/com/buddy2/buddy2/config/ChatController.java
@@ -0,0 +1,15 @@
+package com.buddy2.buddy2.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ChatController {
+
+    @GetMapping("/chat/opened/health")
+    public String health(){
+        return "hello world";
+    }
+}
