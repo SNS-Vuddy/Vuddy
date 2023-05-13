07f5d599b0d1c413f55632b75db802ff80f1d3ea
ryuwc <wom05169@gmail.com>
2023-05-13 14:40:27 +0900

    feat: 내 친구 조회 api 수정

--- src/main/java/com/edu/ssafy/friend/model/repository/FriendRepository.java
+++ src/main/java/com/edu/ssafy/friend/model/repository/FriendRepository.java
@@ -1,16 +1,11 @@
 package com.edu.ssafy.friend.model.repository;
 
-import com.edu.ssafy.friend.model.dto.AllFriendDto;
 import com.edu.ssafy.friend.model.entity.User;
 import com.edu.ssafy.friend.model.entity.UserFriends;
 import com.edu.ssafy.friend.model.entity.enums.UserFriendStatus;
 import com.edu.ssafy.friend.model.repository.custom.FriendRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface FriendRepository extends JpaRepository<UserFriends, Long>, FriendRepositoryCustom {
@@ -19,9 +14,5 @@ public interface FriendRepository extends JpaRepository<UserFriends, Long>, Frie
 
 
     UserFriends findByRequestUserAndReceiveUser(User requester, User receiver);
-
-
-    @Query("select new com.edu.ssafy.friend.model.dto.AllFriendDto(u.nickname, u.profileImage) from UserFriends uf join uf.receiveUser u where uf.requestUser = :requester and uf.status = :status order by u.nickname asc")
-    List<AllFriendDto> findAllByRequestUserAndStatusIs(@Param("requester") User requester, @Param("status") UserFriendStatus status);
 
 }
