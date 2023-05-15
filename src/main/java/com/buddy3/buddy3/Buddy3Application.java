6a1b16456d8d2ca93871438c34d2784dc8954695
dokkyunYU <ydgar001@naver.com>
2023-05-15 09:29:24 +0900

    feat : dbscan 클러스터링 알고리즘 추가

--- src/main/java/com/buddy3/buddy3/Buddy3Application.java
+++ src/main/java/com/buddy3/buddy3/Buddy3Application.java
@@ -2,8 +2,12 @@ package com.buddy3.buddy3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableAsync
 public class Buddy3Application {
 
 	public static void main(String[] args) {
