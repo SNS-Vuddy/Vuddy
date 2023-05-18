d4737cafa7d9a143f1743ac46fce45fa3db045fc
HyunseokCheong <hyunseokcheong@gmail.com>
2023-05-19 08:48:04 +0900

    feat: logout

--- client/Vuddy/app/src/main/java/com/b305/vuddy/view/fragment/SettingFragment.kt
+++ client/Vuddy/app/src/main/java/com/b305/vuddy/view/fragment/SettingFragment.kt
@@ -1,19 +1,24 @@
 package com.b305.vuddy.view.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import com.b305.vuddy.R
 import com.b305.vuddy.databinding.FragmentSettingBinding
+import com.b305.vuddy.service.ImmortalService
+import com.b305.vuddy.util.SharedManager
 import com.b305.vuddy.viewmodel.UserDataViewModel
 
 @Suppress("DEPRECATION")
 class SettingFragment : Fragment() {
 
     private lateinit var binding : FragmentSettingBinding
-
+    val sharedManager :SharedManager by lazy { SharedManager(requireContext()) }
 //    private lateinit var userData: UserResponse
 
     private lateinit var userViewModel : UserDataViewModel
@@ -48,6 +53,14 @@ class SettingFragment : Fragment() {
 //        }
 //
 //        userViewModel.userData.postValue(userData)
+        binding.btnLogout.setOnClickListener {
+            sharedManager.removeCurrentToken()
+            sharedManager.removeCurrentUser()
+            sharedManager.removeChatRoomList()
+            sharedManager.removeChatList()
+            requireActivity().stopService(Intent(requireContext(), ImmortalService::class.java))
+            it.findNavController().navigate(R.id.action_settingFragment_to_signupActivity)
+        }
 
         return binding.root
     }
