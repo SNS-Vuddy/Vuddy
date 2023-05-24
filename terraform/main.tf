842d997abf95f7c63794ec0625f28b1335d92875
김태훈 <hoan123432@naver.com>
2023-05-24 09:54:52 +0900

    Update terraform/main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,10 +11,6 @@ terraform {
 
 provider "aws" {
   region = "us-east-2"
-
-  
-  access_key = "${AWS_ACCESS_KEY_ID}"
-  secret_key = "${AWS_SECRET_ACCESS_KEY}"
 }
 
 resource "aws_instance" "create_ec2_1" {
